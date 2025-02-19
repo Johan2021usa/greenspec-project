f96a2aee7232dc6eb12ccf5c8ad27cf509bbb63d
JohanLasso <lassojairo@gmail.com>
2025-02-19 12:58:01 -0500

    0.0.6.3 Feat(Gallery component bg completed 30%)

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "../../banner/banner.component";
+import { GalleryComponent } from "../../gallery/gallery.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent],
+  imports: [BannerComponent, GalleryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
