05ada9b497eed08f7c3093c19b27cda443988b62
JohanLasso <lassojairo@gmail.com>
2025-02-21 12:53:14 -0500

    Feat(Gallery scroll x added for desktop, pending on mobile)

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CardComponent } from "./card/card.component";
 
 @Component({
@@ -8,28 +8,56 @@ import { CardComponent } from "./card/card.component";
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
-export class GalleryComponent {
+export class GalleryComponent implements AfterViewInit{
+
+  @ViewChild('wrapperGallery') wrapperGallery!: ElementRef;
+
+  options:ScrollToOptions = {
+    top: 0,
+    left: 290,
+    behavior: "smooth"
+  }
 
   cardsInfo = [
     {
-      "title": "Greenspec's Gateway to Innovation",
+      "title": "Greenspec's Gateway to Innovation #1",
       "category" : "Technology",
       "description" : "Lorem ipsum dolor sit amet consectetur. Porttitor est mauris curabitur pellentesque."
     },
     {
-      "title": "Greenspec's Gateway to Innovation",
+      "title": "Greenspec's Gateway to Innovation #2",
       "category" : "Technology",
       "description" : "Lorem ipsum dolor sit amet consectetur. Porttitor est mauris curabitur pellentesque."
     },
     {
-      "title": "Greenspec's Gateway to Innovation",
+      "title": "Greenspec's Gateway to Innovation #3",
       "category" : "Technology",
       "description" : "Lorem ipsum dolor sit amet consectetur. Porttitor est mauris curabitur pellentesque."
     },
     {
-      "title": "Greenspec's Gateway to Innovation",
+      "title": "Greenspec's Gateway to Innovation #4",
       "category" : "Technology",
       "description" : "Lorem ipsum dolor sit amet consectetur. Porttitor est mauris curabitur pellentesque."
     },
-  ]
+    {
+      "title": "Greenspec's Gateway to Innovation #5",
+      "category" : "Technology",
+      "description" : "Lorem ipsum dolor sit amet consectetur. Porttitor est mauris curabitur pellentesque."
+    },
+    {
+      "title": "Greenspec's Gateway to Innovation #6",
+      "category" : "Technology",
+      "description" : "Lorem ipsum dolor sit amet consectetur. Porttitor est mauris curabitur pellentesque."
+    }
+  ];
+
+  ngAfterViewInit(): void {
+
+  }
+
+  scrollImage(direction:boolean){
+    const wrapperGallery:HTMLElement = this.wrapperGallery.nativeElement;
+    this.options.left = (direction) ? (290) : (-290);
+    wrapperGallery.scroll(this.options);
+  }
 }
