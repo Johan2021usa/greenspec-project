18b50477272ff5d2fe013a04aaa7d5912323fd63
JohanLasso <lassojairo@gmail.com>
2025-02-22 12:29:42 -0500

    0.0.16.5 Feat(insights board completed for desktop and mobile)

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BannerComponent } from "../../banner/banner.component";
 import { GalleryComponent } from "../../gallery/gallery.component";
 import { InfoBoardComponent } from "../../info-board/info-board.component";
+import { InsightsComponent } from "../../insights/insights.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent, GalleryComponent, InfoBoardComponent],
+  imports: [BannerComponent, GalleryComponent, InfoBoardComponent, InsightsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
