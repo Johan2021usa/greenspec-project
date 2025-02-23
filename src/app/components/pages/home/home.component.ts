8c3416fe6a994a80227130fe5b22222926e0b466
JohanLasso <lassojairo@gmail.com>
2025-02-23 00:28:08 -0500

    0.0.18.6 Feat(partners brand completed for desktop and mobiel)

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { BannerComponent } from "../../banner/banner.component";
 import { GalleryComponent } from "../../gallery/gallery.component";
 import { InfoBoardComponent } from "../../info-board/info-board.component";
 import { InsightsComponent } from "../../insights/insights.component";
+import { PartnersComponent } from "../../partners/partners.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent, GalleryComponent, InfoBoardComponent, InsightsComponent],
+  imports: [BannerComponent, GalleryComponent, InfoBoardComponent, InsightsComponent, PartnersComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
